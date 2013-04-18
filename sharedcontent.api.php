0fda9946856cf810dbd7744e13e9e06cdaee3437
corvus_ch <christian.haeusler@md-systems.ch>
2013-04-18 08:33:31 +0200

    Issue #1941784 by corvus_ch | Berdir: Fixed entity_created() and entity_updated() not set in tests.

--- sharedcontent.api.php
+++ sharedcontent.api.php
@@ -11,6 +11,63 @@
  */
 
 /**
+ * Allow to alter index records when they get created or updated.
+ *
+ * @param $index
+ *   The index record.
+ * @param $wrapper
+ *   An entity wrapper object containing the entity the index is created for.
+ * @param $op
+ *   'create' if a new index record was created, 'update' otherwise.
+ */
+function hook_sharedcontent_index_alter($index, $wrapper, $op) {
+  if ($wrapper->type() == 'node') {
+    $node = $wrapper->value();
+    $index->keywords = sharedcontent_rules_get_keywords($node, 'node');
+    $index->tags = sharedcontent_rules_get_tags($node, 'node');
+    switch ($node->status) {
+      case NODE_PUBLISHED:
+        $index->status = SHAREDCONTENT_INDEX_STATUS_VISIBLE;
+        break;
+      default:
+        $index->status = SHAREDCONTENT_INDEX_STATUS_LINKABLE;
+    }
+
+    $terms = taxonomy_get_term_by_name($op, 'sharedcontent_reason');
+    if (empty($terms)) {
+      $vocabulary = taxonomy_vocabulary_machine_name_load('sharedcontent_reason');
+      if (!$vocabulary) {
+        $vocabulary = entity_create('taxonomy_vocabulary', array(
+          'name' => 'Sharedcontent reason',
+          'machine_name' => 'sharedcontent_reason',
+          'description' => 'Reasons for why a shared Content Index record exists.',
+          'module' => 'sharedcontent',
+        ));
+        taxonomy_vocabulary_save($vocabulary);
+      }
+      $term = entity_create('taxonomy_term', array(
+        'name' => drupal_ucfirst($op),
+        'description' => '',
+        'parent' => array(0),
+        'vid' => $vocabulary->vid,
+      ));
+      taxonomy_term_save($term);
+    }
+    else {
+      $term = reset($terms);
+    }
+
+    $index->field_sharedcontent_reason = array(
+      LANGUAGE_NONE => array(
+        array(
+          'tid' => $term->tid,
+        )
+      )
+    );
+  }
+}
+
+/**
  * Allow to alter the keywords stored in the sharedcontent_index.
  *
  * @param $keywords
