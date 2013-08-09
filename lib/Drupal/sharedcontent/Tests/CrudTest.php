acb4d2b8eb5228da5a7c29be0872b326fcf9235f
corvus_ch <christian.haeusler@md-systems.ch>
2013-08-09 18:57:23 +0200

    Minimal CRUD test for index

---
+++ lib/Drupal/sharedcontent/Tests/CrudTest.php
@@ -0,0 +1,42 @@
+<?php
+ 
+/**
+ * @file
+ * Contains \Drupal\sharedcontent\Tests\CrudTest.
+ */
+ 
+namespace Drupal\sharedcontent\Tests;
+ 
+use Drupal\simpletest\DrupalUnitTestBase;
+
+class CrudTest extends DrupalUnitTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('user', 'system', 'field', 'sharedcontent');
+
+  static function getInfo() {
+    return array(
+      'name' => 'CRUD tests',
+      'description' => 'Basic crud operations for indexes and ',
+      'group' => 'Shared Content',
+    );
+  }
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->installSchema('sharedcontent', array('sharedcontent_index'));
+  }
+
+  /**
+   * Test CRUD functions for the index.
+   */
+  public function testIndex() {
+    $index = entity_create('sharedcontent_index', array());
+    $this->assertEqual(SAVED_NEW, $index->save());
+  }
+}
