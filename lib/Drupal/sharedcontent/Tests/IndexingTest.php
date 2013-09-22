fa538e78655d1b06f552ced90e042d728aea9050
corvus_ch <christian.haeusler@md-systems.ch>
2013-09-22 16:38:00 +0200

    Rename node to entity
    Will be unified pattern when more indexing tests follow.

--- lib/Drupal/sharedcontent/Tests/IndexingTest.php
+++ lib/Drupal/sharedcontent/Tests/IndexingTest.php
@@ -56,38 +56,38 @@ class IndexingTest extends DrupalUnitTestBase {
     Index::setIndexable('node', 'indexable', FALSE);
 
     // When I create a new entity of type 'node' with bundle 'indexed'.
-    $node = entity_create('node', array(
+    $entity = entity_create('node', array(
       'title' => 'Not indexed node',
       'type' => 'indexed',
     ));
-    $node->save();
+    $entity->save();
 
     // Then no index record was created.
-    $this->assertFalse(sharedcontent_index_exists($node), 'No index record was created.');
+    $this->assertFalse(sharedcontent_index_exists($entity), 'No index record was created.');
 
     // Given bundle 'indexed' of entity 'node' is 'enabled' for indexing.
-    Index::setIndexableByEntity($node, TRUE);
+    Index::setIndexableByEntity($entity, TRUE);
 
     // When I create a new entity of type 'node' with bundle 'indexed'.
-    $node = entity_create('node', array(
+    $entity = entity_create('node', array(
       'title' => 'Indexed node',
       'type' => 'indexed',
     ));
-    $node->save();
+    $entity->save();
 
     // Then a new index record was created.
-    $this->assertTrue(sharedcontent_index_exists($node), 'Found index record for created node.');
+    $this->assertTrue(sharedcontent_index_exists($entity), 'Found index record for created node.');
 
     // And the created index matches the values from the indexed 'node'.
-    $index = sharedcontent_index_load_by_entity($node);
+    $index = sharedcontent_index_load_by_entity($entity);
     $this->assertEqual($index->getConnectionName(), NULL, 'The connection name is empty.');
     $this->assertEqual($index->getChangedTime(), REQUEST_TIME, 'The index record was last changed within this request.');
     $this->assertEqual($index->getCreatedTime(), REQUEST_TIME, 'The index record was created within this request.');
-    $this->assertEqual($index->getEntityChangedTime(), $node->getChangedTime(), 'The changed time matches.');
-    $this->assertEqual($index->getEntityCreatedTime(), $node->getCreatedTime(), 'The created time matches.');
+    $this->assertEqual($index->getEntityChangedTime(), $entity->getChangedTime(), 'The changed time matches.');
+    $this->assertEqual($index->getEntityCreatedTime(), $entity->getCreatedTime(), 'The created time matches.');
     $this->assertEqual($index->getEntityType(), 'node', 'The entity type matches.');
     $this->assertEqual($index->getEntityBundle(), 'indexed', 'The entity bundle matches.');
-    $this->assertEqual($index->getEntityUuid(), $node->uuid(), 'The indexed id matches.');
+    $this->assertEqual($index->getEntityUuid(), $entity->uuid(), 'The indexed id matches.');
     $this->assertEqual($index->getKeywords(), NULL, 'The keywords are empty.');
     $this->assertEqual($index->getLangcode(), Language::LANGCODE_DEFAULT, 'The language is undefined.');
     $this->assertEqual($index->getParentUuid(), NULL, 'The index has no parent.');
@@ -95,14 +95,14 @@ class IndexingTest extends DrupalUnitTestBase {
     $this->assertEqual($index->getTags(), NULL, 'The tags are empty.');
     $this->assertEqual($index->getTitle(), 'Indexed node', 'The title matches.');
     $this->assertEqual($index->getTranslationSetId(), '', 'The translation set id is empty.');
-    $node_uri = $node->uri();
+    $node_uri = $entity->uri();
     $this->assertTrue(preg_match("|{$node_uri['path']}$|", $index->getUrl()), 'The translation set id is empty.');
 
     // When I delete the node.
-    $node->delete();
+    $entity->delete();
 
     // Then the status of the index is set to "not reachable;..
-    $index = sharedcontent_index_load_by_entity($node);
+    $index = sharedcontent_index_load_by_entity($entity);
     $this->assertEqual($index->getStatus(), IndexInterface::STATUS_NOT_REACHABLE);
   }
 }
