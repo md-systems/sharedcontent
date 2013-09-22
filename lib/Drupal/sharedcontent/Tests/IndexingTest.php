ea3eae528bdc7a83e7791f3094d654b2504f8c89
corvus_ch <christian.haeusler@md-systems.ch>
2013-09-22 18:01:02 +0200

    Added term indexing test

--- lib/Drupal/sharedcontent/Tests/IndexingTest.php
+++ lib/Drupal/sharedcontent/Tests/IndexingTest.php
@@ -229,4 +229,75 @@ class IndexingTest extends DrupalUnitTestBase {
     $index = sharedcontent_index_load_by_entity($entity);
     $this->assertEqual($index->getStatus(), IndexInterface::STATUS_NOT_REACHABLE, 'Index hat status not reachable.');
   }
+
+  /**
+   * Test indexing for terms.
+   */
+  public function testIndexingTerm() {
+    $this->enableModules(array('taxonomy'));
+    $this->installSchema('taxonomy', array(
+      'taxonomy_term_data',
+      'taxonomy_term_hierarchy',
+      'taxonomy_index',
+    ));
+
+    $vocabulary = entity_create('taxonomy_vocabulary', array(
+      'vid' => 'test_vocab',
+      'name' => 'Test vocabulary',
+    ));
+    $vocabulary->save();
+
+    // Given bundle 'user' of entity 'user' is 'not enabled' for indexing.
+    Index::setIndexable('taxonomy_term', 'test_vocab', FALSE);
+
+    // When I create a new entity of type 'user' with bundle 'user'.
+    $entity = entity_create('taxonomy_term', array(
+      'name' => 'Non indexed term',
+      'vid' => $vocabulary->id(),
+    ));
+    $entity->save();
+
+    // Then no index record was created.
+    $this->assertFalse(sharedcontent_index_exists($entity), 'No index record was created.');
+
+    // Given bundle 'user' of entity 'user' is 'enabled' for indexing.
+    Index::setIndexableByEntity($entity, TRUE);
+
+    // When I create a new entity of type 'node' with bundle 'indexed'.
+    $entity = entity_create('taxonomy_term', array(
+      'name' => 'Indexed term',
+      'vid' => $vocabulary->id(),
+    ));
+    $entity->save();
+
+    // Then a new index record was created.
+    $this->assertTrue(sharedcontent_index_exists($entity), 'Found index record for created node.');
+
+    // And the created index matches the values from the indexed 'node'.
+    $index = sharedcontent_index_load_by_entity($entity);
+
+    $this->assertEqual($index->getConnectionName(), NULL, 'The connection name is empty.');
+    $this->assertEqual($index->getChangedTime(), REQUEST_TIME, 'The index record was last changed within this request.');
+    $this->assertEqual($index->getCreatedTime(), REQUEST_TIME, 'The index record was created within this request.');
+    $this->assertEqual($index->getEntityChangedTime(), $entity->getChangedTime(), 'The changed time matches.');
+    $this->assertEqual($index->getEntityCreatedTime(), REQUEST_TIME, 'The created time matches.');
+    $this->assertEqual($index->getEntityType(), 'taxonomy_term', 'The entity type matches.');
+    $this->assertEqual($index->getEntityBundle(), 'test_vocab', 'The entity bundle matches.');
+    $this->assertEqual($index->getEntityUuid(), $entity->uuid(), 'The indexed id matches.');
+    $this->assertEqual($index->getKeywords(), NULL, 'The keywords are empty.');
+    $this->assertEqual($index->getLangcode(), Language::LANGCODE_DEFAULT, 'The language is undefined.');
+    $this->assertEqual($index->getParentUuid(), NULL, 'The index has no parent.');
+    $this->assertEqual($index->getStatus(), IndexInterface::STATUS_VISIBLE, 'The index record has status visible.');
+    $this->assertEqual($index->getTags(), NULL, 'The tags are empty.');
+    $this->assertEqual($index->getTitle(), 'Indexed term', 'The title matches.');
+    $node_uri = $entity->uri();
+    $this->assertTrue(preg_match("|{$node_uri['path']}$|", $index->getUrl()), 'The translation set id is empty.');
+
+    // When I delete the node.
+    $entity->delete();
+
+    // Then the status of the index is set to "not reachable;..
+    $index = sharedcontent_index_load_by_entity($entity);
+    $this->assertEqual($index->getStatus(), IndexInterface::STATUS_NOT_REACHABLE, 'Index hat status not reachable.');
+  }
 }
