090313f70798d33ccc0748b9243373299afc2e18
corvus_ch <christian.haeusler@md-systems.ch>
2013-09-26 16:48:56 +0200

    Added patch method

--- lib/Drupal/sharedcontent/Plugin/rest/resource/SharedContentResource.php
+++ lib/Drupal/sharedcontent/Plugin/rest/resource/SharedContentResource.php
@@ -113,4 +113,72 @@ class SharedContentResource extends EntityResource {
       throw new HttpException(500, t('Internal Server Error'), $e);
     }
   }
+
+  /**
+   * Responds to entity PATCH requests.
+   *
+   * @param mixed $uuid
+   *   The entity ID.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The HTTP response object.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
+   * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
+   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+   */
+  public function patch($uuid, EntityInterface $entity = NULL) {
+    if ($entity == NULL) {
+      throw new BadRequestHttpException(t('No entity content received.'));
+    }
+
+    if (empty($uuid)) {
+      throw new NotFoundHttpException();
+    }
+
+    $definition = $this->getPluginDefinition();
+    if ($entity->entityType() != $definition['entity_type']) {
+      throw new BadRequestHttpException(t('Invalid entity type'));
+    }
+
+    $original_entity = entity_load_by_uuid($definition['entity_type'], $uuid);
+    // We don't support creating entities with PATCH, so we throw an error if
+    // there is no existing entity.
+    if ($original_entity == FALSE) {
+      throw new NotFoundHttpException();
+    }
+
+    if (!$original_entity->access('update')) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Overwrite the received properties.
+    foreach ($entity as $field_name => $field) {
+      if (isset($entity->{$field_name})) {
+        if ($field->isEmpty() && !$original_entity->get($field_name)->access('delete')) {
+          throw new AccessDeniedHttpException(t('Access denied on deleting field @field.', array('@field' => $field_name)));
+        }
+        $original_entity->set($field_name, $field->getValue());
+        if (!$original_entity->get($field_name)->access('update') || !in_array($field_name, $original_entity->getExposedFields())) {
+          throw new AccessDeniedHttpException(t('Access denied on updating field @field.', array('@field' => $field_name)));
+        }
+      }
+    }
+
+    // Validate the received data before saving.
+    $this->validate($original_entity);
+    try {
+      $original_entity->save();
+      watchdog('rest', 'Updated entity %type with UUID %uuid.', array('%type' => $entity->entityType(), '%uuid' => $entity->uuid()));
+
+      // Update responses have an empty body.
+      return new ResourceResponse(NULL, 204);
+    }
+    catch (EntityStorageException $e) {
+      throw new HttpException(500, t('Internal Server Error'), $e);
+    }
+  }
 }
